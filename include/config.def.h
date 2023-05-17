7a2e55648c391739029523535ac8f6694d090091
Leonard <>
2023-05-17 13:26:23 +0200

    smaller font

--- include/config.def.h
+++ include/config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 // static char *font = "Fira Code:pixelsize=11:antialias=true:autohint=true";
-static char *font = "FiraCode Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:pixelsize=11:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
