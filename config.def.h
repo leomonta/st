6b0b198b4aef0316f1653ed70d776f231edbfa5a
Leonard <>
2023-05-17 13:21:58 +0200

    using fish + truecolor on deafult

--- config.def.h
+++ config.def.h
@@ -16,7 +16,7 @@ static int borderpx = 2;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/bin/fish";
+static char *shell = "/usr/local/bin/start_fish";
 char *utmp = NULL;
 /* scroll program: to enable use a string like "scroll" */
 char *scroll = NULL;
