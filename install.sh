699ad95fc9b5a3bacbd0f10370fd737231163891
Leonard <>
2023-05-17 13:26:23 +0200

    install script fixes and gitignore

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp bin/st /usr/local/bin/
+cp -f bin/st /usr/local/bin/
 chmod 755 /usr/local/bin/st
-sed "s/VERSION/0.9/g" < st.1 > /usr/local/share/man1/st.1
-chmod 644 /urs/local/share/man1/st.1
+sed "s/VERSION/0.9/g" < st.1 > /usr/local/share/man/man1/st.1
+chmod 644 /usr/local/share/man/man1/st.1
 tic -sx st.info
 echo Please see the README file regarding the terminfo entry of st.
 mkdir -p /usr/local/share/applications
