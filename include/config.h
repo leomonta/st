beab15a6b0ac41c8ad287f832fb66416a09dc73f
Leonard <>
2023-05-17 13:26:23 +0200

    font stuff, i guess 2; nerd font boogaloo

--- include/config.h
+++ include/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:pixelsize=11:antialias=true:autohint=true";
-// static char *font = "FiraCode Nerd Font:pixelsize=11:antialias=true:autohint=true";
+// static char *font = "Fira Code:pixelsize=11:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
