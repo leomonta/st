ec59edfe1b50302e6dabd4914e72efd5ada1f48b
Leonard <remon.w.dev@gmail.com>
2023-08-15 17:03:23 +0200

    small changes

--- include/config.def.h
+++ include/config.def.h
@@ -6,16 +6,17 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 // static char *font = "Fira Code:pixelsize=11:antialias=true:autohint=true";
-static char *font = "FiraCode Nerd Font Mono:pixelsize=11:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
+// border from the edge of the screen end whe to start writing
 static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
+ * 0: value of shell in config.h
  * 1: program passed with -e
  * 2: scroll and/or utmp
  * 3: SHELL environment variable
  * 4: value of shell in /etc/passwd
- * 5: value of shell in config.h
  */
 static char *shell = "/usr/local/bin/start_fish";
 char *utmp = NULL;
@@ -145,7 +146,7 @@ static unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 4;
 
 /*
  * Default columns and rows numbers
@@ -199,8 +200,8 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ TERMMOD,              XK_KP_Add,      zoom,           {.f = +1} },
+	{ TERMMOD,              XK_KP_Subtract, zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
