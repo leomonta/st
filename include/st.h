3fbf59e2a1807f1c375f868451a3e055adc11c5d
Leonard <remon.w.dev@gmail.com>
2023-08-18 22:11:45 +0200

    working alt tab icon

--- include/st.h
+++ include/st.h
@@ -1,5 +1,6 @@
 /* See LICENSE for license details. */
 
+#include <gd.h>
 #include <stdint.h>
 #include <sys/types.h>
 #include <wchar.h>
--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ chmod 755 /usr/local/bin/st
 sed "s/VERSION/0.9LM/g" < st.1 > /usr/local/share/man/man1/st.1
 chmod 644 /usr/local/share/man/man1/st.1
 tic -sx st.info
+cp -f st.png /usr/local/share/pixmaps/st.png
 echo Please see the README file regarding the terminfo entry of st.
 mkdir -p /usr/local/share/applications
 cp -f st.desktop /usr/local/share/applications
