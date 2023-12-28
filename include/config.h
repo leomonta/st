f03a77ba0dc3590e1383a54a9f7c4c472fa30468
Leonard <remon.w.dev@gmail.com>
2023-12-28 15:24:02 +0100

    updated compilation process

--- include/config.h
+++ include/config.h
@@ -35,6 +35,10 @@ static float chscale = 1.0;
  * word delimiter string
  *
  * More advanced example: L" `'\"()[]{}"
+ * space 
+ * all quotes
+ * all brackets
+ * the · is for nvim space visualization
  */
 wchar_t *worddelimiters = L" `'\"()[]{·";
 
--- make
+++ make
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo cpp_builder.py -a -p release
+
+sudo ./clean_and_setup.sh
+
+cpp_builder.py -a -p release
+
+sudo ./install.sh
