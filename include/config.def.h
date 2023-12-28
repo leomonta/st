1da289336e2e9a363975dca58bc5a86af7353740
Leonard <remon.w.dev@gmail.com>
2023-12-28 18:10:52 +0100

    omfg god it was the nerd font's fault, kill me

--- include/config.def.h
+++ include/config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-// static char *font = "Fira Code:pixelsize=11:antialias=true:autohint=true";
-static char *font = "FiraCode Nerd Font:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Fira Code:pixelsize=11:antialias=true:autohint=true";
+// static char *font = "FiraCode Nerd Font:pixelsize=12:antialias=true:autohint=true";
 // border from the edge of the screen end whe to start writing
 static int borderpx = 2;
 
@@ -29,7 +29,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 0.9;
+static float chscale = 1.0;
 
 /*
  * word delimiter string
