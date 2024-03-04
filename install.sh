566dbc7b3e74d25b5f945b2015b88eb3985fe5ae
Leonard <remon.w.dev@gmail.com>
2024-03-04 16:53:31 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 cp -f bin/st /usr/local/bin/
-chmod 755 /usr/local/bin/st
-sed "s/VERSION/0.9.1-LM/g" < st.1 > /usr/local/share/man/man1/st.1
-chmod 644 /usr/local/share/man/man1/st.1
+sudo chmod 755 /usr/local/bin/st
+sudo sed "s/VERSION/0.9.1-LM/g" < st.1 > /usr/local/share/man/man1/st.1
 tic -sx st.info
-cp -f st.png /usr/share/icons/st.png
+sudo cp -f st.png /usr/share/icons/st.png
 echo Please see the README file regarding the terminfo entry of st.
-mkdir -p /usr/local/share/applications
-cp -f st.desktop /usr/local/share/applications
+sudo mkdir -p /usr/local/share/applications
+sudo cp -f st.desktop /usr/local/share/applications
