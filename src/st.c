f8854d3a77470fa54f9dc73a3611e92cf26307b4
Leonard <remon.w.dev@gmail.com>
2024-03-12 09:21:06 +0100

    updated to latest st change

--- src/st.c
+++ src/st.c
@@ -1596,7 +1596,7 @@ void csihandle(void) {
 			ttywrite(vtiden, strlen(vtiden), 0);
 		break;
 	case 'b': /* REP -- if last char is printable print it <n> more times */
-		DEFAULT(csiescseq.arg[0], 1);
+		LIMIT(csiescseq.arg[0], 1, 65535);
 		if (term.lastc)
 			while (csiescseq.arg[0]-- > 0)
 				tputc(term.lastc);
